9861a07d3a90080875ad2241398833a06ea28e41
Saysa Bounkhong <saysabou@gmail.com>
2021-09-16 11:12:54 +0200

    defines available METHOD regarding operations

--- src/Entity/Renter.php
+++ src/Entity/Renter.php
@@ -7,7 +7,10 @@ use App\Repository\RenterRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     collectionOperations={"get"},
+ *     itemOperations={"get"}
+ * )
  * @ORM\Entity(repositoryClass=RenterRepository::class)
  */
 class Renter
