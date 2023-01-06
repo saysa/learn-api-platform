2c93c72272fc646af0ff2d57e2d3daf103767c3c
Saysa Bounkhong <saysa.bounkhong@infolox.de>
2023-01-06 18:22:07 +0100

    activate login with json

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login", methods={"POST"})
+     */
+    public function login(): Response
+    {
+        return $this->json([
+            'user' => $this->getUser() ? $this->getUser()->getId() : null,
+        ]);
+    }
+}
