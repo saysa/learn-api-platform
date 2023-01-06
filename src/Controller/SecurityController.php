0120ea8e97e62b1963ee8c8bf2bea9ca72bb82ef
Saysa Bounkhong <saysa.bounkhong@infolox.de>
2023-01-06 18:45:30 +0100

    return User IRI when auth success

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use ApiPlatform\Core\Api\IriConverterInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +14,10 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login", methods={"POST"})
      */
-    public function login(): Response
+    public function login(IriConverterInterface $iriConverter): Response
     {
-        return $this->json([
-            'user' => $this->getUser() ? $this->getUser()->getId() : null,
+        return new Response(null, 204, [
+            'Location' => $iriConverter->getIriFromItem($this->getUser()),
         ]);
     }
 }
