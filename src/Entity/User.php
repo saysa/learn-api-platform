7442b0c80e1d047a2ab2bff960d413ff601c29d1
Saysa Bounkhong <saysa.bounkhong@infolox.de>
2023-01-07 18:17:35 +0100

    encode the plainPassword with a DataPersister

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,6 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -65,6 +66,7 @@ class User implements UserInterface
 
     /**
      * @Groups({"user:write"})
+     * @SerializedName("password")
      */
     private $plainPassword;
 
